4cb3c079a79475ddacca5b0d10504542511057cc
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-09-08 23:45:16 +0900

    now, which shortest path algo to use...

--- so_long/so_long/check_map_pt_iii.c
+++ so_long/so_long/check_map_pt_iii.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   check_map_pt_iii.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/04 07:48:07 by myoshika          #+#    #+#             */
-/*   Updated: 2022/09/08 23:09:21 by myoshika         ###   ########.fr       */
+/*   Created: 2022/09/08 22:00:20 by myoshika          #+#    #+#             */
+/*   Updated: 2022/09/08 22:01:12 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "so_long.h"
 
-# include <stdbool.h>
-# include <stdlib.h>
-
-# define WRONG_MAP_SIZE 3
-# define INVALID_MAP_FORMATTING 4
-# define MALLOC_FAIL 5
-typedef struct s_game{
-	char	**map;
-	size_t	num_of_collectibles;
-	size_t	num_of_players;
-	size_t	num_of_exits;
-	size_t	map_width;
-	size_t	map_height;
-}	t_game;
-
-#endif
+static void	check_for_valid_path(t_game *info)
+{
+	
+}
--- test.c
+++ test.c
@@ -20,17 +20,26 @@ int main()
 	char **test;
 
 	test = malloc(sizeof(char *) * 5);
-	*test = malloc(5);
-	while (*test)
+	for (int i = 0; i++; i < 5)
 	{
 		*test = malloc(5);
-		printf("%p\n", *test);
-		*test++ = malloc(3);
-		*test = "ab";
-		*test--;
-		printf("%p\n", *(test + 1));
+		*test = "abcd";
 		*test++;
+	}
+	while (test)
+	{
+		printf("1: %p\n", *test);
+		*test++;
+		printf("2: %p\n", *test);
+		*test--;
+		printf("3: %p\n", *test);
+		printf("4: %p\n", *(test + 1));
+		test++;
 	}
 	//didnt make a good test but just wanted to confirm that doing ++ moves by the datatype size
 	//so hypothetically, i would have to (test + <size> * sizeof(whicheverdatatype))
+
+	// 0x55b774877004
+	// 0x55b77487700c
+	// so a difference of 8 bits. 
 }
