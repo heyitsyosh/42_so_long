fc41ea772b8a98e5b1a40948b5cb893d895026ce
maria <winterzaq1@gmail.com>
2022-10-03 04:58:02 +0900

    update

---
+++ so_long/srcs/mandatory/read_check_map/extract_map.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extract_map.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 21:59:30 by myoshika          #+#    #+#             */
+/*   Updated: 2022/10/03 03:10:37 by myoshika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/so_long.h"
+
+static void	init_l(t_line *l)
+{
+	l->pre = 0;
+	l->mid = 0;
+	l->post = 0;
+}
+
+static size_t	skip_irrelevant(char *ptr, int call, t_parse *p)
+{
+	const char	*map_chars = "10CEP\n";
+	size_t		i;
+
+	i = 0;
+	while (!ft_strchr(map_chars, ptr[i]))
+		i++;
+	if ((ptr[i] == '\n' && call == PRE) || !ptr[i])
+		p->map_ended = true;
+	return (i);
+}
+
+static size_t	parse_map(char *ptr, t_line *l, t_parse *p, t_game *g)
+{
+	const char	*map_chars = "10CEP";
+	char		*current_char;
+	size_t		i;
+
+	i = 0;
+	while (ptr[i])
+	{
+		current_char = ft_strchr(map_chars, ptr[i]);
+		if (!current_char)
+			break ;
+		else if (*current_char == 'C')
+			g->num_of_collectibles++;
+		else if (*current_char == 'E')
+			g->num_of_exits++;
+		else if (*current_char == 'P')
+			g->num_of_players++;
+		i++;
+	}
+	if (g->map_height == 0)
+		g->map_width = i;
+	else
+		if (g->map_width != i)
+			g->map_error = INVALID_MAP_FORMATTING;
+	return (i);
+}
+
+static char	*parse_line(t_line *l, t_parse *p, t_game *g)
+{
+	l->pre = skip_irrelevant(l->current_ln, PRE, p);
+	l->mid = parse_map(l->current_ln + l->pre, l, p, g);
+	l->post = skip_irrelevant(l->current_ln + l->pre + l->mid, POST, p);
+	if (!p->map_ended)
+		if (l->pre != p->col_offset || l->mid != g->map_width)
+			g->map_error = INVALID_MAP_FORMATTING;
+	g->map_height++;
+	return (ft_substr(l->current_ln + l->pre, 0, l->mid));
+}
+
+void	extract_map(t_parse *p, t_game *g)
+{
+	char	*current_ln;
+	char	*extracted;
+	t_line	l;
+
+	l.current_ln = p->map_start_ptr;
+	extracted = ft_strdup("");
+	while (!p->map_ended && g->map_error == MAP_OK)
+	{
+		init_l(&l);
+		current_ln = parse_line(&l, p, g);
+		l.current_ln += l.pre + l.mid + l.post;
+		extracted = ft_strjoin_with_free(extracted, current_ln, 1);
+		extracted = ft_strjoin_with_free(extracted, "\n", 1);
+	}
+	check_num_of_cep(g);
+	check_if_rectangle(g);
+	check_basic_requirements(g);
+	if (g->map_error == MAP_OK)
+	{	
+		g->map = ft_split(extracted, '\n');
+		if (!g->map)
+			g->map_error = MALLOC_FAIL;
+	}
+	free(extracted);
+}
---
+++ so_long/srcs/mandatory/read_check_map/read_map.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 16:10:46 by myoshika          #+#    #+#             */
+/*   Updated: 2022/10/03 04:52:21 by myoshika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/so_long.h"
+
+static void	print_map_error(t_game *g)
+{
+	size_t	i;
+
+	if (g->map_error == WRONG_MAP_SHAPE)
+		print_err_and_exit("wrong map shape");
+	else if (g->map_error == INVALID_MAP_FORMATTING)
+		print_err_and_exit("invalid map formatting");
+	else if (g->map_error == MALLOC_FAIL)
+		print_err_and_exit("malloc failure");
+	else if (g->map_error == NO_MAP)
+		print_err_and_exit("no map in file");
+	else if (g->map_error == WRONG_CEP)
+		print_err_and_exit("invalid number of player, exit, and collectibles");
+	i = 0;
+	while (g->map[i])
+	{
+		free(g->map[i]);
+		i++;
+	}
+	free(g->map);
+	if (g->map_error == NO_WALLS)
+		print_err_and_exit("no walls");
+	if (g->map_error == NO_VALID_PATH)
+		print_err_and_exit("map not playable");
+}
+
+static void	init_g_and_p(t_parse *p, t_game *g)
+{
+	g->num_of_collectibles = 0;
+	g->num_of_players = 0;
+	g->num_of_exits = 0;
+	g->map_width = 0;
+	g->map_height = 0;
+	g->map_error = MAP_OK;
+	p->map_ended = false;
+	p->col_offset = 0;
+}
+
+static int	open_map(char *map_file)
+{
+	int		fd;
+
+	fd = open(map_file, O_RDONLY);
+	if (fd == -1)
+		print_err_and_exit("failed to open file");
+	return (fd);
+}
+
+static char	*read_map_into_line(int fd)
+{
+	char	*line;
+	char	*joined_line;
+
+	joined_line = ft_strdup("");
+	while (joined_line)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		joined_line = ft_strjoin_with_free(joined_line, line, FREE_FIRST_PARAM);
+		free(line);
+	}
+	if (!joined_line)
+		print_err_and_exit("malloc error");
+	return (joined_line);
+}
+
+void	get_map(char *map_file, t_game *g)
+{
+	int		fd;
+	int		line_status;
+	char	*joined_line;
+	t_parse	p;
+
+	fd = open_map(map_file);
+	joined_line = read_map_into_line(fd);
+	init_p_and_g(&p, g);
+	if (g->map_error == MAP_OK)
+		skip_before_map(joined_line, &p, g);
+	if (g->map_error == MAP_OK)
+		extract_map(&p, g);
+	if (g->map_error == MAP_OK)
+		skip_after_map(&p, g);
+	free(joined_line);
+	check_for_walls(g);
+	check_for_valid_path(g);
+	if (g->map_error != MAP_OK)
+		print_map_error(g);
+}
