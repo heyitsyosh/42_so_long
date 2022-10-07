757253053dbd4538f6bfd54db6d25a2ad488d57b
maria <winterzaq1@gmail.com>
2022-10-08 04:54:23 +0900

    update

---
+++ so_long/bonus/close_game_bonus.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   close_game_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 18:03:06 by myoshika          #+#    #+#             */
+/*   Updated: 2022/10/08 02:09:57 by myoshika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long_bonus.h"
+
+void	destroy_images(t_game *g)
+{
+	mlx_destroy_image(g->mlx_id, g->i->coin_front);
+	mlx_destroy_image(g->mlx_id, g->i->coin_left);
+	mlx_destroy_image(g->mlx_id, g->i->coin_right);
+	mlx_destroy_image(g->mlx_id, g->i->coin_side);
+	mlx_destroy_image(g->mlx_id, g->i->empty);
+	mlx_destroy_image(g->mlx_id, g->i->wall);
+	mlx_destroy_image(g->mlx_id, g->i->exit);
+	mlx_destroy_image(g->mlx_id, g->i->p_front);
+	mlx_destroy_image(g->mlx_id, g->i->p_left);
+	mlx_destroy_image(g->mlx_id, g->i->p_right);
+	mlx_destroy_image(g->mlx_id, g->i->portal_front);
+	mlx_destroy_image(g->mlx_id, g->i->portal_left);
+	mlx_destroy_image(g->mlx_id, g->i->portal_right);
+	mlx_destroy_image(g->mlx_id, g->i->i_left);
+	mlx_destroy_image(g->mlx_id, g->i->i_right);
+	mlx_destroy_image(g->mlx_id, g->i->ii);
+	mlx_destroy_image(g->mlx_id, g->i->iii);
+	mlx_destroy_image(g->mlx_id, g->i->iv);
+	mlx_destroy_image(g->mlx_id, g->i->v);
+	mlx_destroy_image(g->mlx_id, g->i->vi);
+	mlx_destroy_image(g->mlx_id, g->i->vii);
+	mlx_destroy_image(g->mlx_id, g->i->viii);
+}
+
+int	close_game(t_game *g)
+{
+	free_map(g);
+	destroy_images(g);
+	mlx_destroy_window(g->mlx_id, g->win_id);
+	mlx_destroy_display(g->mlx_id);
+	free(g->mlx_id);
+	free(g->i);
+	exit(0);
+	return (0);
+}
---
+++ so_long/bonus/map_parse_utils_bonus.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_parse_utils_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 14:10:10 by myoshika          #+#    #+#             */
+/*   Updated: 2022/10/08 02:10:32 by myoshika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long_bonus.h"
+
+void	info_to_g(char chr, size_t i, t_game *g)
+{
+	if (chr == 'P')
+	{
+		g->num_of_players++;
+		g->player_y = g->map_height;
+		g->player_x = i;
+	}
+	else
+	{
+		g->num_of_exits++;
+		g->exit_y = g->map_height;
+		g->exit_x = i;
+	}
+}
+
+void	free_map(t_game *g)
+{
+	size_t	i;
+
+	i = 0;
+	while (g->map[i])
+	{
+		free(g->map[i]);
+		i++;
+	}
+	free(g->map);
+}
+
+void	print_map_error(t_game *g)
+{
+	if (g->map_error == WRONG_MAP_SHAPE)
+		print_err_and_exit("wrong map shape");
+	else if (g->map_error == INVALID_MAP_FORMATTING)
+		print_err_and_exit("invalid map formatting");
+	else if (g->map_error == MALLOC_FAIL)
+		print_err_and_exit("malloc failure");
+	else if (g->map_error == NO_MAP)
+		print_err_and_exit("no map in file");
+	else if (g->map_error == WRONG_CEP)
+		print_err_and_exit("invalid number of player, exit, and collectibles");
+	free_map(g);
+	if (g->map_error == NO_WALLS)
+		print_err_and_exit("no walls");
+	if (g->map_error == NO_VALID_PATH)
+		print_err_and_exit("map not playable");
+}
