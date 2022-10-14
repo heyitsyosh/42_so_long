5338811b2bd8542fa09fd46173e740712257d628
maria <winterzaq1@gmail.com>
2022-10-15 05:26:33 +0900

    update

--- so_long/bonus/check_args_bonus.c
+++ so_long/bonus/check_args_bonus.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 22:26:04 by myoshika          #+#    #+#             */
-/*   Updated: 2022/10/08 05:36:31 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/10/15 05:08:27 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ void	check_args(int argc, char **argv)
 	char	*pointer_to_dot;
 
 	if (argc != 2)
-		print_err_and_exit("invalid arguments");
-	if (ft_strcmp(argv[0], "./so_long"))
 		print_err_and_exit("invalid arguments");
 	pointer_to_dot = ft_strrchr(argv[1], '.');
 	if (!pointer_to_dot)
