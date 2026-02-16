0ac0e533851f50c7907b3633b98598f6b3306e3b
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-02-16 17:45:53 +0100

    [fix]: Pass va_arg as pointer in printf

--- so_long/lib/printf/ft_printf.c
+++ so_long/lib/printf/ft_printf.c
@@ -15,7 +15,7 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-size_t	conversion(const char *specifiers, t_info *info, va_list args)
+size_t	conversion(const char *specifiers, t_info *info, va_list *args)
 {
 	int		printed;
 
@@ -27,15 +27,15 @@ size_t	conversion(const char *specifiers, t_info *info, va_list args)
 			&& info->precision >= INT_MAX - 2))
 		return (INT_MAX);
 	if (info->fmt == 'c')
-		printed += put_char(info, va_arg(args, int));
+		printed += put_char(info, va_arg(*args, int));
 	else if (info->fmt == 's')
-		printed += put_str(info, va_arg(args, char *), false);
+		printed += put_str(info, va_arg(*args, char *), false);
 	else if (info->fmt == 'p')
-		printed += put_unsigned(info, va_arg(args, unsigned long long));
+		printed += put_unsigned(info, va_arg(*args, unsigned long long));
 	else if (info->fmt == 'd' || info->fmt == 'i')
-		printed += put_signed(info, ft_itoa(va_arg(args, int)));
+		printed += put_signed(info, ft_itoa(va_arg(*args, int)));
 	else if (ft_strchr("uxX", info->fmt))
-		printed += put_unsigned(info, va_arg(args, unsigned int));
+		printed += put_unsigned(info, va_arg(*args, unsigned int));
 	else if (info->fmt == '%')
 		printed += put_char(info, '%');
 	return (printed);
@@ -68,7 +68,7 @@ int	ft_printf(const char *input, ...)
 	{
 		init(info);
 		if (input[info->i] == '%')
-			printed += conversion(input + (++info->i), info, args);
+			printed += conversion(input + (++info->i), info, &args);
 		else
 			printed += no_conversion(input + info->i, info);
 	}
